d957745772eb423a085a9e6ebf23c2fb91f4fdc2
Stark, John <john.stark@lindab.com>
2025-07-11 13:15:46 +0200

    rom parameter required unless debug flag is given

--- src/emulator/options.rs
+++ src/emulator/options.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct EmulatorOptions {
-    #[arg(short, long, default_value = "", help = "Path to the .nes ROM file")]
+    #[arg(short, long, default_value = "", help = "Path to the .nes ROM file", required_unless_present("debug"))]
     pub rom: String,
     
     #[arg(short, long, help = "Enable debug mode with nestest.nes")]
